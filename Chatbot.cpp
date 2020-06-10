b05e2259ecd3d7d0bbb2936970d7b7f1b5b684d9
jpinasanz <jpinasanz@sdsu.edu>
2020-06-09 18:03:13 -0700

    added comments to main

--- Chatbot.cpp
+++ Chatbot.cpp
@@ -1,2 +1,4 @@
 #include "Chatbot.h"
 
+
+
--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include "Chatbot.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 using namespace std;
 
 int main(int argc, char **argv){
+    pid_t pid = getpid();
     return 0;
 }
+
+//Create file QUOTE.txt
+// Open file, write PID into file, close file.
+//Create Semaphore 
