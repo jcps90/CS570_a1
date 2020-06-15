c374d6d4939b73a6b31b2cd310795e65d33bd3e5
CS-Buran <62851064+CS-Buran@users.noreply.github.com>
2020-06-15 13:06:57 -0700

    Update main.h
    added header

--- main.h
+++ main.h
@@ -1,3 +1,10 @@
+
+// Authors: Juan Pina-Sanz, Mikhail Mineev
+// Users: cssc2147, cssc2160
+// Class: CS 570, Summer 2020
+// Assignment 1
+// Filename: main.h
+
 #include <pthread.h>
 #include <stdio.h>
 #include <iostream>
